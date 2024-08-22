f7f64da6e65009de83570921331f734ffa1aed20
Epikman-yes11 <davidberaia11@gmail.com>
2024-08-22 16:10:24 +0400

    commit

---
+++ Level 28/classwork/classwork.py
@@ -0,0 +1,40 @@
+
+
+txt = "hello world"
+
+x =txt.upper()
+
+
+print(x)
+
+
+
+txt1 = "HELLO WORLD"
+
+x = txt.lower()
+
+print(x)
+
+
+txt2 = "hELLO WORLD"
+
+x = txt2.capitalize()
+
+
+print(x)
+
+
+txt3 = "Hello World"
+
+x =txt3.swapcase()
+
+print(x)
+
+
+
+txt4 = "Hello World"
+
+x = txt4.find("World")
+
+
+print(x)
---
+++ Level 28/homework/homework.py
@@ -0,0 +1,62 @@
+
+
+txt = "hello world"
+
+x =txt.upper()
+
+
+txt1 = "HELLO WORLD"
+
+x = txt.lower()
+
+print(x)
+
+
+
+txt2 = "hELLO WORLD"
+
+x = txt2.capitalize()
+
+
+print(x)
+
+
+txt3 = "Hello World"
+
+x =txt3.swapcase()
+
+print(x)
+
+txt4 = "Hello World"
+
+x = txt4.find("World")
+
+
+print(x)
+
+
+list =["orange,apple,mandarin"]
+
+
+x = len(list)
+
+print(x)
+
+
+list1 = ["orange.apple.mandarin"]
+
+x =list1.append("cherry")
+
+
+
+
+list2 =["orange,apple,mandarin"]
+
+x =list2.insert("cherry")
+
+print(x)
+
+
+list3 =["orange,apple,mandarin"]
+
+x = list3.pop("orange")
