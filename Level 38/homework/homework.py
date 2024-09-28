09f0e5ac4cd2081a2c6e8e8f3a05bf91008e6623
Epikman-yes11 <davidberaia11@gmail.com>
2024-09-28 23:42:19 +0400

    commit

---
+++ Level 38/homework/homework.py
@@ -0,0 +1,9 @@
+
+ls = ["H","E","L","L","O","W","O","R","L","D"]
+print(ls)
+
+ls = ["H","E","L","L","O","W","O","R","L","D"]
+print("-".join(ls))
+
+ls = ["H","E","L","L","O","W","O","R","L","D"]
+print("_".join(ls))
