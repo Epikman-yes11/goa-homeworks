91831047d57ae55b7adee2c8ae166e2b45cd1a07
Epikman-yes11 <davidberaia11@gmail.com>
2024-11-16 12:50:11 +0400

    commit

---
+++ Level 51/homework/homework.py
@@ -0,0 +1,10 @@
+
+numbers = [50,52,53,67,98]
+
+def func():
+    return numbers * 2
+
+
+numbers = map(func,(numbers * 2))
+
+print(numbers)
---
+++ Level 51/homework/homework2.py
@@ -0,0 +1,9 @@
+
+odd_numbers_only = [52,53,67,64]
+
+
+def func():
+   return odd_numbers_only //2
+
+
+odd_numbers_only = map(func,(odd_numbers_only // 2))
