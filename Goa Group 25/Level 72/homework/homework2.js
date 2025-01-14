1d89918b1dc0ea9c279a61756a7303e5a0d08ab3
Epikman-yes11 <davidberaia11@gmail.com>
2025-01-14 19:41:04 +0400

    commit

---
+++ Goa Group 25/Level 72/homework/homework2.js
@@ -0,0 +1,14 @@
+
+let Grades = prompt("Celsius:")
+
+if (Grades > 90){
+    console.log("A")
+} else if(Grades > 80){
+    console.log("B")
+}else if(Grades < 70){
+    console.log("C")
+}else if(Grades < 60){
+    console.log("D")
+}else if(Grades < 59){
+    console.log("F")
+}
