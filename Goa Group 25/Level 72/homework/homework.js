f344ba80e7b138dfca2e563f6b3282288a6bac36
Davit Beraia <davidberaia11@gmail.com>
2025-01-22 18:18:00 +0400

    commit

---
+++ Goa Group 25/Level 72/homework/homework.js
@@ -0,0 +1,8 @@
+
+let number = prompt("რიცხვი შემოიტანე")
+
+if (number %2 == 0){
+    console.log("რიცხვი არის ლუწი")
+} else{
+    console.log("რიცხვი არის კენტი")
+}
---
+++ Goa Group 25/Level 74/classwork/classwork.js
@@ -0,0 +1,26 @@
+
+
+let num = Number(prompt('Enter Num:'))
+
+switch(num){
+    case 1:
+        console.log('number is one');
+        break;
+
+    case 2:
+        console.log('number is two');
+        break;
+    case 3:
+        console.log('number is three');
+        break;
+    case 4:
+        console.log('number is four');
+        break;
+    case 4:
+        console.log('number is five');
+        break;
+
+    default: {
+        alert('invalid number')
+    }
+}
