fcd2c3e09497ce56a862fb8e66f5dbee7e13ed1d
Davit Beraia <davidberaia11@gmail.com>
2025-01-26 13:59:02 +0400

    commit

---
+++ Goa Group 25/Level 74/homewrok/homework1.js
@@ -0,0 +1,16 @@
+
+let num  =  prompt('Enter num :')
+
+
+switch(num) {
+
+    case '1':
+        console.log('weather is rainy')
+    case '2':
+        console.log('weather is cloudy')
+    case '3':
+        console.log('weather is windy')
+    default: {
+        alert('weather is sunny')
+    }
+}
---
+++ Goa Group 25/Level 74/homewrok/homework2.js
@@ -0,0 +1,24 @@
+
+let num  =  prompt('Enter num :')
+
+
+switch(num) {
+
+    case '1':
+        console.log('Monday')
+    case '2':
+        console.log('Tuesday')
+    case '3':
+        console.log('Wendsday')
+    case '4':
+        console.log('Thursday')
+    case '5':
+        console.log('Friday')
+    case '6':
+        console.log('Saturday')
+    case '7':
+        console.log('Sunday')
+    default: {
+        alert('weather is sunny')
+    }
+}
